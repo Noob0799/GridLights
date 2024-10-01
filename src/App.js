031a5868d41457f1fdb95076c148f6f5f8355b10
Noob0799 <uchihaitachi0799@gmail.com>
2024-10-02 03:13:15 +0530

    Code changes for Grid Lights

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [record, setRecord] = useState([]);
+  const intervalRef = useRef();
+
+  const handleClick = (index) => {
+    if (index != 4 && !record.includes(index)) {
+      const newRecord = [...record, index];
+      setRecord(newRecord);
+    }
+  };
+
+  useEffect(() => {
+    if(record.length === 0) {
+      clearInterval(intervalRef.current);
+    } else if (record.length === 8) {
+      intervalRef.current = setInterval(() => {
+        setRecord(record => record.slice(0, record.length - 1));
+      }, 300);
+    }
+  }, [record]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Grid Lights</h1>
+      <div className="grid-lights">
+        {Array(9)
+          .fill(null)
+          .map((_, index) => (
+            <div
+              key={index}
+              className={`${index != 4 ? "grid-cell" : ""} ${record.includes(index) ? "fill" : ""}`}
+              onClick={() => handleClick(index)}
+            ></div>
+          ))}
+      </div>
     </div>
   );
 }
